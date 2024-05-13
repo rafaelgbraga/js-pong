2be9885c8adb7a429f77b1bc2398a284d01446b2
Rafael Gonçalves Braga <166659019+rafaelgbraga@users.noreply.github.com>
2024-05-13 19:00:36 -0300

    sketch.js

---
+++ sketch.js
@@ -0,0 +1,27 @@
+    rect(450, 10, 40, 20);
+    fill(255);
+    text(pontosDoOponente, 470, 26);
+
+
+
+}
+
+
+function marcaPonto() {
+    if (xBolinha > 590) {
+        meusPontos += 1;
+        ponto.play();
+    }
+    if (xBolinha < 10) {
+        pontosDoOponente += 1;
+        ponto.play();
+    }
+}
+
+
+function preload(){
+  trilha = loadSound("trilha.mp3");
+  ponto = loadSound("ponto.mp3");
+  raquetada = loadSound("raquetada.mp3");
+}
+
